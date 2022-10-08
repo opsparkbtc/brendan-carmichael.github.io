abbf92e422441462c21118417370a93173f99436
Brendan Thomas Carmichael <brendantcarmichael@gmail.com>
2022-10-08 22:19:04 +0000

    17:17 10/8/22

---
+++ studies/string-manipulation.js
@@ -0,0 +1,22 @@
+/** String Manipulation:
+ * 
+ * 0. Strings are data types in Javascript consisting of alphanumeric characters places together. We have various methods
+ * with which to alter them, which will be listed and explained below.
+ * 
+ * 
+ */
+
+//1. Length
+
+//Accessing and being aware of the length property of a string is foundational to many manipulation methods.
+//While it is not a method of manipulation itself, understanding it is vital.
+//Here we print a string's length to the console:
+
+let exampleString = 'This is the example'
+console.log(exampleString) //prints 19 to the console. Spaces are characters.
+
+//2.0 Manipulation Methods
+//Here we list some of the various methods for manipulating strings
+//
+//2.1 
+
